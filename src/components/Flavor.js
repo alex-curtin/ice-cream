9cb169755d39420d484b949e0587600f987694f3
ahcurtin <acurtin@theride-edp.com>
2020-08-21 15:13:04 -0400

    minor styling, update favicon

--- src/components/Flavor.js
+++ src/components/Flavor.js
@@ -12,7 +12,10 @@ const Flavor = ({ flavor }) => {
       data-sal-easing="ease"
     >
       <div className={styles.imgContainer}>
-        <Image fluid={flavor.image.fluid} />
+        <Image
+          fluid={flavor.image.fluid}
+          style={{ filter: "drop-shadow(2px 2px 3px rgba(0, 0, 0, 0.15))" }}
+        />
         <div className={styles.flavorCardInfo}>
           <GrIceCream size="3.5rem" />
           {flavor.description.description}
